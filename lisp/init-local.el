8e86b0f658844ed94a8e1a1477999512ceb007b2
shirui <1229408499@qq.com>
2017-11-24 13:43:15 +0800

    make scheme-mode better

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,6 +1,19 @@
-(add-hook 'scheme-mode-hook (lambda () (paredit-mode t)))
 (setq auto-save-default nil)
 (setq frame-resize-pixelwise t)
+
+
+
+(defun shirui/add-pretty-lambda ()
+  "Make some word or string show as pretty Unicode symbols."
+  (setq prettify-symbols-alist
+        '(
+          ("lambda" . 955)
+          )))
+
+(add-hook 'scheme-mode-hook 'shirui/add-pretty-lambda)
+(add-hook 'scheme-mode-hook (lambda () (paredit-mode t)))
+
+
 
 (provide 'init-local)
 ;;; init-local.el ends here
